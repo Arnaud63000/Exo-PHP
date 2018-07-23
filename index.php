c42423da7d65ea7421a16e9578bd82db60f25a79
Arnaud63000 <40628189+Arnaud63000@users.noreply.github.com>
2018-07-23 12:20:50 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+$name = "Arnaud";
+echo $name ." ";
+
+
+
+$lastname = 21;
+$firstname = "Arnaud";
+$age = 22;
+echo " ". $lastname . ' ' . $firstname . ' ' . $age ." ";
+
+
+$km = 1;
+echo $km ." ";
+$km = 3;
+echo $km ." ";
+$km = 125;
+echo $km . " ";
+
+
+$nom = "BlaBla";
+$chiffre = 22;
+$chiffre2 = 2.2;
+$nombre = true;
+
+echo $nom ." ". $chiffre ." ". $chiffre2 ." ". $nombre ." " ;
+
+
+
+$chiffre3 = null;
+echo $chiffre3 ." ";
+$chiffre3 = 10;
+echo $chiffre3 ." ";
+
+
+
+$name2 = "Arnaud";
+echo "Bonjour" ." ". $name2 ." ". ",Comment tu va ?";
+
+
+
+$lastname = "Arnaud";
+$firstname = "Blabla";
+$age = 22;
+
+echo "Bonjour" ." ". $lastname ." ". $firstname ." ". ",tu as" ." ". $age ." ". "ans";
+
+
+
+
+$resultat1 = 3+4;
+$resultat2 = 5*20;
+$resultat3 = 45/5;
+
+echo " ".$resultat1 ." ". $resultat2 ." ". $resultat3 ;
+?>
